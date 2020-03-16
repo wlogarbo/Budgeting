78ca7381a51a1d7978e2af28ccdecade953ac82d
Wayne Logarbo <wlogarbo@gmail.com>
2020-03-16 14:43:39 -0500

    Added dacpac

---
+++ src/BudgetingDb/dbo/Tables/BudgetItems.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[BudgetItems] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50)  NULL,
+    [Description] NVARCHAR (MAX) NOT NULL,
+    [Amount]      MONEY          NOT NULL,
+    [Frequency]   NVARCHAR (50)  NOT NULL,
+    [StartDate]   DATETIME       NOT NULL,
+    [EndDate]     DATETIME       NULL,
+    [Type]        NVARCHAR (50)  NOT NULL
+);
+
---
+++ src/BudgetingDb/dbo/Tables/TestItems.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TestItems] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50) NULL,
+    [Description] NVARCHAR (50) NULL
+);
+
